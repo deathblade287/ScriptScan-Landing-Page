a61826046dc606083e803fe1d7ed86ea9ee3809a
Aviral Dhingra <deathblade287@192.168.1.24>
2023-07-04 11:05:40 +0530

    Draft 1 - Hero + Nav

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Link } from "react-scroll";
+import logo32 from "./imgs/icon32.png";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function NavLink({ children, ...props }) {
+  return (
+    <Link
+      activeClass="active"
+      spy={true}
+      smooth={true}
+      offset={-70}
+      duration={500}
+      className="hover:text-blue-500 cursor-pointer"
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <header>
+      <nav
+        className="fixed top-0 text-white p-6 shadow-lg rounded-2xl bg-red my-7 mx-9"
+        style={{ width: "calc(100% - 2*2.25rem)" }}
+      >
+        <div className="flex justify-between w-full items-center">
+          <div className="flex items-center justify-center">
+            <img src={logo32} alt="logo" className="w-10 h-auto" />
+          </div>
+          <div className="flex justify-around w-1/2 mx-auto items-center">
+            <NavLink to="section2">Section 2</NavLink>
+            <NavLink to="section3">Section 3</NavLink>
+            <NavLink to="section4">Section 4</NavLink>
+          </div>
+          <div className="flex items-center justify-center">
+            <a
+              href="https://chrome.google.com/webstore/detail/your-extension-id"
+              className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+            >
+              Download
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <div className="hero">
+      <div className="title_desc">
+        <h1>Script Scan</h1>
+        <h2>Code to natural language</h2>
+        <br />
+        <p>
+          Summarizes code files hosted on github so that you don't have to waste
+          time reading before you start contributing.
+        </p>
+        <br />
+        <br />
+        <a
+          href="https://chrome.google.com/webstore/detail/your-extension-id"
+          className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+        >
+          Download Extension
+        </a>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      white: "#D3CFC9",
+      red: "#F9385C",
+      slate: "#181a1b",
+      blue: "#61dafb",
+    },
+  },
+  plugins: [],
+};
