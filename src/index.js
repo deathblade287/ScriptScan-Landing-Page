961bb0da7e0b5422410e400043dd420378905a8a
Aviral Dhingra <71305878+deathblade287@users.noreply.github.com>
2024-08-14 18:08:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ function NavBar() {
             </div>
             <div className="flex items-center justify-center">
               <a
-                href="https://chrome.google.com/webstore/detail/script-scan/kljkclbeopgkgkilideimbkdjihdlkag/related"
+                href="https://github.com/deathblade287/Script-Scan"
                 className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
               >
                 Download
